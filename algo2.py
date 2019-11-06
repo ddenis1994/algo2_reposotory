a15521c6527b77fcfdc46dae8ca9e35b8dcc38c4
denis <ddenis1994@gmail.com>
2019-11-06 20:24:48 +0200

    add some comments to BFS

--- algo2.py
+++ algo2.py
@@ -11,7 +11,7 @@ class point:
 
 class grafh:
     v=list()
-    E=list()
+    E={}
 
 
 
@@ -21,19 +21,22 @@ def bfs(g,s):
     pi={}
     d={}
     color={}
-    
+    # first init the data structure for the BFS to work curractly
     for i in g.v:
         pi[i.id]=None
         d[i.id]=None
         color[i.id]='WHITE'
-    
+    #init the first stage in the queue
     Q = deque([s])
     d[s.id]=0
     color[s.id]="GRAY"
-
+    # while there is somting in the Queue 
     while Q:
+        #start by poping the first element in the queue
         u=Q.popleft()
+        #for each point the connects to u do the fullowing
         for j in E[u.id]:
+            #first chack if the node allrady in the procees
             if color[j.id]=="WHITE":
                 d[j.id]=d[u.id]+1
                 pi[j.id]=u.id
